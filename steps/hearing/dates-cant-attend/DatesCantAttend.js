e2fe2f6b32ce2be58002c62c1501f5883a8726ea
bendiggle <ben.diggle5@gmail.com>
2018-04-11 14:50:16 +0100

    add error cllback

--- steps/hearing/dates-cant-attend/DatesCantAttend.js
+++ steps/hearing/dates-cant-attend/DatesCantAttend.js
@@ -12,7 +12,9 @@ const UKBankHolidays = require('@hmcts/uk-bank-holidays');
 
 const bankHolidaysMiddleware = async (req, res, next) => {
     const ukBankHolidays = new UKBankHolidays(['england-and-wales']);
-    await ukBankHolidays.loadBankHolidayDates();
+    await ukBankHolidays.loadBankHolidayDates(error => {
+        next(error);
+    });
     res.locals.ukBankHolidays = ukBankHolidays;
     next();
 };
